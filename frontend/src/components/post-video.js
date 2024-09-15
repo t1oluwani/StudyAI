cc95424e7c2316d28709f90c6d94cc5f2398b84c
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-09-15 17:08:52 -0600

    Embed youtube vid functionality

--- frontend/src/components/post-video.js
+++ frontend/src/components/post-video.js
@@ -4,10 +4,22 @@ import VideoTranscipt from './sub-components/video-transcript';
 import ChatScreen from './sub-components/chat-screen';
 
 function PreVideo({ link }) {
+
+  function convertToEmbedLink(link) {
+    const vidId = link.split('v=')[1]?.split('&')[0];
+
+    if (vidId) {
+      return `https://www.youtube.com/embed/${vidId}`;
+    } else {
+      console.error('Invalid YouTube URL');
+      return '';
+    }
+  }
+
   return (
     <div className="post-video">
       <div className="video-side">
-        <EmbeddedVideo url={link} />
+        <EmbeddedVideo url={convertToEmbedLink(link)} />
         <VideoTranscipt/>
       </div>
       <div className="chat-side">
