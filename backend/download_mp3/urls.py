38039cd2dee8ba80f35d9cbb39727ae24084573b
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-09-17 00:13:00 -0600

    change backend from Django to FastAPI

--- backend/download_mp3/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('download_mp3/', views.file_drop, name='File Drop'),
-]
--- backend/download_mp3/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def file_drop(request):
-      return render(request, 'file_drop.html')
-
-      
-
