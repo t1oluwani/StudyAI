8122c10869dc7e4888628040192551a243cc7f22
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-10-05 13:59:39 -0600

    curr

--- backend/test/post_transcript.py
+++ backend/test/post_transcript.py
@@ -5,6 +5,6 @@ api_url = "http://127.0.0.1:8000/transcribe/"
 
 print(f"Attempting to post transcript to firebase database...")
 
-response = requests.post(api_url, params={"title": "BLEACH 20th Anime Anniversary Official Trailer ｜ BLEACH ｜ VIZ.mp3"})
+response = requests.post(api_url, params={"title": "DAN DA DAN OP ｜ Otonoke by Creepy Nuts ｜ Netflix Anime.mp3"})
 
 print(response.json())
