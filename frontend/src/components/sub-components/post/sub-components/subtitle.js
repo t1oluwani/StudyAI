c21f9a85202af2bc3e54db829d0d46b4223981a8
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-10-10 13:27:49 -0600

    New Subtitle component and Transcipt showing

---
+++ frontend/src/components/sub-components/post/sub-components/subtitle.js
@@ -0,0 +1,10 @@
+function Subtitle({ timestamp, subtitle }) {
+
+  return (
+    <a className="subtitle-text">
+      {subtitle}
+    </a>
+  );
+}
+
+export default Subtitle;
--- frontend/src/components/sub-components/post/video-transcript.js
+++ frontend/src/components/sub-components/post/video-transcript.js
@@ -1,38 +1,29 @@
 import "../../../styling/video-side.css";
+import Subtitle from "./sub-components/subtitle";
 
 function VideoTranscript({ transcript }) {
-
-  console.log("Transcript:", transcript);
 
   const toDictionary = (array) => { // Convert the transcript array to a dictionary
     if (!(Array.isArray(array))) {
       return {};
     }
-    
+
     return array.reduce((dictionary, { start, text }) => {
       dictionary[start] = text;
       return dictionary;
     }, {});
   }
 
-  const displayTranscript = (transcriptArray) => { // Display the transcript in a readable format
-    if (!transcriptArray) {
-      return;
-    }
-
-    console.log("Array:", transcriptArray);
-    const transcriptDict = toDictionary(transcriptArray);
-    console.log("Dictionary:", transcriptDict);
-
-    return;
-  };
-
-  const displayedTranscript = displayTranscript(transcript)
+  const transcriptDict = toDictionary(transcript);
 
   return (
     <div className="video-transcript">
       <h3>Video Transcript</h3>
-      <p>{displayedTranscript || "No Transcript Provided"}</p>
+      <div className="subtitles">
+        {Object.entries(transcriptDict).map(([start, text]) => (
+          <Subtitle timestamp={start} subtitle={text}/>
+        ))}
+      </div>
     </div>
   );
 }
