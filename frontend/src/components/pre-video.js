9044915058e7cefa826997e89ed88062c33af909
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-10-12 23:42:09 -0600

    Thematic styling

--- frontend/src/components/pre-video.js
+++ frontend/src/components/pre-video.js
@@ -1,12 +1,17 @@
 import '../styling/pre-video.css';
 import FileDrop from './sub-components/pre/file-drop';
 import LinkPaste from './sub-components/pre/link-paste';
+import StudyAILogo from '../logos/StudyAI Logo (No Bg).png';
 
 function PreVideo({ setLink, setFile, setTranscriptStatus, setLoadingState }) {
   return (
     <div className="pre-video">
-      <FileDrop setFile={setFile} setTranscriptStatus={setTranscriptStatus} setLoadingState={setLoadingState}/>
-      <LinkPaste setLink={setLink} setTranscriptStatus={setTranscriptStatus} setLoadingState={setLoadingState}/>
+      <div className="header-container">
+        <img src={StudyAILogo} alt="StudyAI Logo" className="logo" />
+        <h1> StudyAI </h1>
+      </div>
+      <FileDrop setFile={setFile} setTranscriptStatus={setTranscriptStatus} setLoadingState={setLoadingState} />
+      <LinkPaste setLink={setLink} setTranscriptStatus={setTranscriptStatus} setLoadingState={setLoadingState} />
     </div>
   );
 }
